90ab97c30af95adf67af02a61dc20855d1714030
Brad McGonigle <brad@bradmcgonigle.com>
2021-02-18 20:08:43 -0500

    Add typescript and theme-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ThemeProvider } from 'theme-ui'
+import theme from './themes/_theme'
+
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default Application
